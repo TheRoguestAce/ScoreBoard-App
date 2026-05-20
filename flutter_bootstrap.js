d247a3522a87181628ce9de70fd96e4fdc9455f7
jeremy <johnnychen762@gmail.com>
2026-05-19 18:14:51 -0700

    send help

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2720543003"
+    serviceWorkerVersion: "1090253158"
   }
 });
